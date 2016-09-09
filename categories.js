27107ca2a6a56e15f08df7f00c47a25f79a6d2bb
Bryan Hankins <bryanhankins@me.com>
2016-09-08 23:10:27 -0500

    products dynamically added to caregories

--- categories.js
+++ categories.js
@@ -1,10 +1,13 @@
 var Original = (function(categoriesAndProducts) {
+    var currentProduct;
+    var currentCategories;
+    var returnProducts=[]
 
   function loadProducts () {
     var data = JSON.parse(this.response);
     var contentEl = document.getElementById("output")
     var productData = "<div class='container'>";
-    var currentProduct;
+
     for (var i = 0; i < data.products.length; i++) {
       currentProduct = data.products[i]
       console.log(currentProduct)
@@ -20,31 +23,25 @@ var Original = (function(categoriesAndProducts) {
     }
 
     contentEl.innerHTML = productData;
+
   }
 
-
-
-  var myRequest = new XMLHttpRequest();
-  myRequest.addEventListener("load", loadProducts);
-  myRequest.open("GET", "products.JSON")
-  myRequest.send();
-
-
-  function loadCategories (pass) {
+  function loadCategories () {
     var catData = JSON.parse(this.response);
     var categoriesEl = document.getElementById("output-categories")
     var categoriesData = "<div class='container'>";
-    var currentCategories;
     for (var j = 0; j < catData.categories.length; j++) {
-      currentCategories = catData.categories[j]
+      currentCategories = catData.categories[ j]
       console.log(currentCategories)
 
       categoriesData += "<ul class='collapsible' data-collapsible='accordion'>"
       categoriesData += "<li>"
       categoriesData += `<div class='collapsible-header'>${currentCategories.name}</div>`
       categoriesData += "<div class='collapsible-body'>"
-      categoriesData += `<p>${pass.currentProduct}</p>`
+      categoriesData += `<div class ='row'><div class='col s12 m4 offset-m1 card light-green card-content white-text'><span class='card-title'>${currentProduct.name}</span>;`
+      categoriesData += `<p></p>`
       categoriesData += "</div></li></ul>";
+
 
       categoriesEl.innerHTML = categoriesData;
 
@@ -56,6 +53,11 @@ var Original = (function(categoriesAndProducts) {
     return categoriesAndProducts;
   }
 
+
+  var myRequest = new XMLHttpRequest();
+  myRequest.addEventListener("load", loadProducts);
+  myRequest.open("GET", "products.JSON")
+  myRequest.send();
 
   var secondRequest = new XMLHttpRequest();
   secondRequest.addEventListener("load", loadCategories);
