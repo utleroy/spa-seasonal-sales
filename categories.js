4374d8b4ce4c94f51e9d636ed5f2fff888189656
Bryan Hankins <bryanhankins@me.com>
2016-09-09 22:25:21 -0500

    looping through products inside of the category sections

--- categories.js
+++ categories.js
@@ -1,28 +1,17 @@
 var Original = (function(categoriesAndProducts) {
     var currentProduct;
     var currentCategories;
-    var returnProducts=[]
+    var data = [];
 
   function loadProducts () {
-    var data = JSON.parse(this.response);
-    var contentEl = document.getElementById("output")
+    data = JSON.parse(this.response);
+    var contentEl = document.getElementById("output");
     var productData = "<div class='container'>";
 
     for (var i = 0; i < data.products.length; i++) {
       currentProduct = data.products[i]
       console.log(currentProduct)
-
-      productData += "<div class ='row'>"
-      productData += "<div class='col s12 m6"
-      productData += " card blue-grey darken-1 card-content white-text'>";
-      productData += `<span class='card-title'>${currentProduct.name}</span>`;
-      productData += `<p>${currentProduct.price}</p>`;
-      productData += `<p>${currentProduct.category_id}</p>`;
-      productData += "</div>";
-      productData += "</div>";
     }
-
-    contentEl.innerHTML = productData;
 
   }
 
@@ -31,17 +20,18 @@ var Original = (function(categoriesAndProducts) {
     var categoriesEl = document.getElementById("output-categories")
     var categoriesData = "<div class='container'>";
     for (var j = 0; j < catData.categories.length; j++) {
-      currentCategories = catData.categories[ j]
-      console.log(currentCategories)
+      currentCategories = catData.categories[j];
 
-      categoriesData += "<ul class='collapsible' data-collapsible='accordion'>"
-      categoriesData += "<li>"
-      categoriesData += `<div class='collapsible-header'>${currentCategories.name}</div>`
-      categoriesData += "<div class='collapsible-body'>"
-      categoriesData += `<div class ='row'><div class='col s12 m4 offset-m1 card light-green card-content white-text'><span class='card-title'>${currentProduct.name}</span>;`
-      categoriesData += `<p></p>`
+
+      categoriesData += "<ul class='collapsible' data-collapsible='accordion'>";
+      categoriesData += "<li>";
+      categoriesData += `<div class='collapsible-header'>${currentCategories.name}</div>`;
+      categoriesData += "<div class='collapsible-body'>";
+      for(var i = 0; i < data.products.length; i++) {
+        categoriesData += `<div class="chip lime">${data.products[i].name}</div><div>Price ${data.products[i].price}</div>`;
+      }
+      categoriesData += `<p></p>`;
       categoriesData += "</div></li></ul>";
-
 
       categoriesEl.innerHTML = categoriesData;
 
@@ -49,19 +39,21 @@ var Original = (function(categoriesAndProducts) {
     });
 
     }
-
+    console.log(data);
     return categoriesAndProducts;
   }
 
 
+
+
   var myRequest = new XMLHttpRequest();
   myRequest.addEventListener("load", loadProducts);
-  myRequest.open("GET", "products.JSON")
+  myRequest.open("GET", "products.JSON");
   myRequest.send();
 
   var secondRequest = new XMLHttpRequest();
   secondRequest.addEventListener("load", loadCategories);
-  secondRequest.open("GET", "categories.JSON")
+  secondRequest.open("GET", "categories.JSON");
   secondRequest.send();
 
 
